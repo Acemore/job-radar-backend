d3da1fcc9f964bdcbb4cbb660342c160df298e89
Acemore <Acemore@users.noreply.github.com>
2026-08-31 17:16:18 +0300

    feat(fetcher): add HeadHunter API fetcher and tests

---
+++ src/fetchers/hh_api.py
@@ -0,0 +1,20 @@
+import httpx
+
+from src.exceptions.fetcher import FetcherNetworkError, FetcherTimeoutError
+from src.utils.retry import retry
+
+HH_VACANCIES_URL = "https://api.hh.ru/vacancies"
+
+
+@retry(attempts=3, delay=1.0)
+async def fetch_hh_vacancies(client: httpx.AsyncClient, query_text: str) -> dict:
+    query_params = {"text": query_text}
+
+    try:
+        response = await client.get(HH_VACANCIES_URL, params=query_params)
+    except httpx.TimeoutException as e:
+        raise FetcherTimeoutError(HH_VACANCIES_URL, e)
+    except httpx.RequestError as e:
+        raise FetcherNetworkError(HH_VACANCIES_URL, e)
+
+    return response.json()
---
+++ tests/test_hh_api_fetcher.py
@@ -0,0 +1,38 @@
+from unittest.mock import AsyncMock, Mock
+
+import pytest
+from httpx import RequestError, TimeoutException
+
+from src.exceptions.fetcher import FetcherNetworkError, FetcherTimeoutError
+from src.fetchers.hh_api import HH_VACANCIES_URL, fetch_hh_vacancies
+
+query_text = "Python"
+
+
+async def test_fetch_hh_vacancies_success(hh_mock_data):
+    client = AsyncMock()
+    response = Mock()
+
+    response.json.return_value = hh_mock_data
+    client.get.return_value = response
+
+    response_json = await fetch_hh_vacancies(client, query_text=query_text)
+
+    assert response_json == hh_mock_data
+    client.get.assert_called_once_with(HH_VACANCIES_URL, params={"text": query_text})
+
+
+async def test_fetch_hh_vacancies_timeout_raises():
+    client = AsyncMock()
+    client.get.side_effect = TimeoutException("Timeout error")
+
+    with pytest.raises(FetcherTimeoutError):
+        await fetch_hh_vacancies(client, query_text=query_text)
+
+
+async def test_fetch_hh_vacancies_network_error_raises():
+    client = AsyncMock()
+    client.get.side_effect = RequestError("Timeout error")
+
+    with pytest.raises(FetcherNetworkError):
+        await fetch_hh_vacancies(client, query_text=query_text)
