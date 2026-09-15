4e453b8fda8acd563ca3d1b88642860d76ee137f
Acemore <Acemore@users.noreply.github.com>
2026-09-15 12:21:52 +0300

    refactor(scraping): remove FetcherTimeoutError and retry decorator

--- src/exceptions/fetcher.py
+++ src/exceptions/fetcher.py
@@ -5,13 +5,6 @@ class FetcherError(JobRadarError):
     pass
 
 
-class FetcherTimeoutError(FetcherError):
-    def __init__(self, url: str, original_exception: Exception):
-        self.url = url
-        self.original_exception = original_exception
-        super().__init__(f"Timeout fetching URL: {url}. Original: {original_exception}")
-
-
 class FetcherNetworkError(FetcherError):
     def __init__(self, url, original_exception):
         self.url = url
